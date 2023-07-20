4407177652b0e4414e996db20efd0262efc6ae93
feralaxe <feralaxe@gmail.com>
2023-07-21 01:24:06 +0600

    objects id and added gitignore

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(dir())
+list = [1, 2, 3, 4, 5]
+
+other_list = list
+
+print(list)
+print(other_list)
+print(id(list))
+print(id(other_list))
+
+list.pop()
+other_list.pop()
+
+print(list)
+print(other_list)
+print(id(list))
+print(id(other_list))
